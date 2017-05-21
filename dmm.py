1f2bf1639df06c3f68a45ebd53b01700559f5042
dbondar <kdfstudio@gmail.com>
2017-05-21 16:27:50 +0200

    DMM for DFT added

--- dmm.py
+++ dmm.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy import linalg
+from scipy import linalg, sparse
 from types import MethodType, FunctionType
 
 
@@ -48,20 +48,26 @@ class DMMK:
             self.beta = 0.
 
         # save the identity matrix
-        self.identity = np.identity(self.H.shape[0], dtype=self.H.dtype)
+        self.identity = sparse.identity(self.H.shape[0], dtype=self.H.dtype)
 
         # First step: symmetrized Hamiltonian
         self.H += self.H.conj().T
         self.H *= 0.5
 
-        # substruct the chemical energy
+        # find eigenvalues of the Hamiltonian for comparision with the exact expression self.get_exact_pop
+        self.E = linalg.eigvalsh(
+            # Convert a sparce Hamiltonian to a dense matrix
+            self.H.todense() if sparse.issparse(self.H) else self.H
+        )
+
+        # substruct the chemical potential
         self.H -= self.mu * self.identity
 
-        # find eigenvalues of the Hamiltonian for comparision with the exact expression self.get_exact_pop
-        self.E = linalg.eigvalsh(self.H)
+        # Save the scaled Hamiltonian for propagation
+        self.scaledH = -0.5 * self.dbeta * self.H
 
         # Set the initial condition for the matrix
-        self.rho = 0.5 * self.identity
+        self.rho = 0.5 * self.identity.todense()
 
     def propagate(self, nsteps=1):
         """
@@ -83,17 +89,20 @@ class DMMK:
             ###########################################################################
 
             # Construct the maps
-            K = self.identity - 0.5 * self.dbeta * self.H.dot(self.identity - self.rho)
+            # K = self.identity - 0.5 * self.dbeta * self.H.dot(self.identity - self.rho)
+            K = self.scaledH.dot(self.identity - self.rho)
+            K += self.identity
 
             self.rho = K.dot(self.rho).dot(K.conj().T)
 
             self.beta += self.dbeta
 
+        # Convert a sparce Hamiltonian to a dense matrix
+        H = (self.H.todense() if sparse.issparse(self.H) else self.H)
+
         print(
             "\nThe norm of the commutator of the obtained density matrix and the Hamiltonian: %.2e\n\n"
-            % linalg.norm(
-                self.rho.dot(self.H) - self.H.dot(self.rho)
-            )
+            % linalg.norm(self.rho.dot(H) - H.dot(self.rho))
         )
 
         return self.rho
@@ -102,14 +111,19 @@ class DMMK:
         """
         :return: The exact Fermi-Dirac density matrix 
         """
-        self.rho_exact = linalg.inv(self.identity + linalg.expm(self.beta * self.H))
+
+        # Convert a sparce Hamiltonian to a dense matrix
+        H = (self.H.todense() if sparse.issparse(self.H) else self.H)
+
+        self.rho_exact = linalg.inv(self.identity + linalg.expm(self.beta * H))
+
         return self.rho_exact
 
     def get_exact_pop(self):
         """
         :return: The exact Fermi-Dirac population distributions
         """
-        return 1. / (1. + np.exp(self.beta * self.E))
+        return 1. / (1. + np.exp(self.beta * (self.E - self.mu)))
 
 ##############################################################################
 #
@@ -125,20 +139,23 @@ if __name__ == '__main__':
         dbeta=0.003,
         mu=-0.9,
         # randomly initialize Hamiltonian (symmetrization will take place in the constructor)
-        H=np.random.normal(size=(40,40)) + 1j * np.random.normal(size=(40,40))
+        H=sparse.random(60, 60, density=0.1),
+        #H=np.random.normal(size=(40,40)) + 1j * np.random.normal(size=(40,40)),
         #np.random.rand(40, 40) + 1j * np.random.rand(40, 40)
     )
 
-    dmm.propagate(30000)
+    dmm.propagate(3000)
 
     plt.title("Population")
 
     p_dmm = linalg.eigvalsh(dmm.rho)[::-1]
 
     plt.plot(dmm.E, p_dmm, '*-', label="DMM")
-    # plt.plot(dmm.E, linalg.eigvalsh(dmm.get_exact_DF())[::-1], '*-', label='exact via expm')
+    plt.plot(dmm.E, linalg.eigvalsh(dmm.get_exact_DF())[::-1], '*-', label='exact via expm')
     plt.plot(dmm.E, dmm.get_exact_pop(), '*-', label='population')
 
     plt.legend()
+    plt.xlabel('Energy')
+    plt.ylabel('Population')
 
     plt.show()
